aacdd5929973366a0f8ee6a84fc3917748fb87c5
Thadeu Penna <thadeupenna@id.uff.br>
2017-05-22 18:36:40 -0300

    Multiplots

--- penduloforcado.py
+++ penduloforcado.py
@@ -53,7 +53,7 @@ class Pendulo:
 		self.energy = 0.5*self.m*(self.l*self.v)**2 + (
 		 self.m*g*self.l*(1-math.cos(self.theta)))
 			
-dt = 1e-2
+dt = 2e-2
 dt2 = dt*dt 
 g = 9.8 
 gamma = 0.5
@@ -64,65 +64,57 @@ p1 = Pendulo(1.,10.,0,math.pi/6)
 
 
 tt=0
-tmax=10*p1.T
+tmax=20*p1.T
 t=np.arange(0,tmax,dt)
 x=np.zeros(t.size)
 v=np.zeros(t.size)
 e=np.zeros(t.size)
 x[0],v[0],e[0]=p1.theta,p1.v,p1.energy
 
-
 for i in range(t.size):
 	p1.move(t[i])
 	x[i],v[i],e[i]=p1.theta,p1.v,p1.energy
 
 
-relax=0
+fig=plt.figure(figsize=(8,8),facecolor='white')
 
-
-
-fig=plt.figure(2,figsize=(8,8),facecolor='white')
-axes = plt.axes(xlim=(-math.pi,math.pi), ylim=(min(v),max(v)))
+phase = fig.add_subplot(211,xlim=(-math.pi,math.pi), ylim=(min(v),max(v)))
 plt.xticks( [-3.14, -3.14/2,0, 3.14/2, 3.14],
         [r'$-\pi$', r'$-\pi/2$','0', r'$+\pi/2$', r'$+\pi$'])
 plt.text(x[0],v[0],'S',color='red')
 plt.xlabel('x')
 plt.ylabel('v')
-line, = axes.plot([], [], '.') 
-timeleg = axes.text(0.02,0.9, '', transform=axes.transAxes)
+line, = phase.plot([], [], '.', markersize=0.6) 
+timeleg = phase.text(0.02,0.9, '', transform=phase.transAxes)
+
+thetaxt = fig.add_subplot(614,xlim=(0,max(t)),ylim=(-math.pi,math.pi))
+line2, = thetaxt.plot([], [], 'r.', markersize=0.2)
+
+vxt = fig.add_subplot(615,xlim=(0,max(t)),ylim=(min(v),max(v)))
+line3, = vxt.plot([], [], 'g-')
+
+ext = fig.add_subplot(616,xlim=(0,max(t)),ylim=(min(e),max(e)))
+line4, = ext.plot([], [], 'b-')
 
 def init():
 	line.set_data([], [])
+	line2.set_data([], [])
+	line3.set_data([], [])
+	line4.set_data([], [])
 	timeleg.set_text('') 
-	return line, timeleg
+	return line, line2, line3, line4, timeleg
 	
 def animate(i):
 	xa = x[:i]
 	ya = v[:i]
 	line.set_data(xa,ya)
+	line2.set_data(t[:i],x[:i])
+	line3.set_data(t[:i],v[:i])
+	line4.set_data(t[:i],e[:i])
 	timeleg.set_text('t = %g' % t[i])
-	return line, timeleg
+	return line, line2, line3, line4, timeleg
 	
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=100000, interval=1e-6, blit=True)
-                              
-                              
-fig=plt.figure(1,facecolor='lightgray')
-fig.canvas.manager.window.wm_geometry('+100+400')
-def graficoxt(pos,f,l,style,mostratics):
-	plt.subplot(pos)
-	axes = plt.gca()
-	axes.axes.get_xaxis().set_visible(mostratics)
-	axes.spines['top'].set_color('none')
-	axes.spines['right'].set_color('none')
-	axes.yaxis.set_ticks_position('left')
-	axes.xaxis.set_ticks_position('bottom')
-	axes.spines['bottom'].set_position(('data',0))
-	plt.ylabel(l)
-	plt.plot(t,f,style)
-
-graficoxt(311,x,'x','r-',False)
-graficoxt(312,v,'y','b-',False)
-graficoxt(313,e,'E','g-',True)
-                              
+                    
 plt.show()
